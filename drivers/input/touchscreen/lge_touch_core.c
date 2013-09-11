0fc3e0117275271a11887eca431407cb05cb513e
faust93 <monumentum@gmail.com>
2013-09-11 11:55:25 +0400

    input: touchscreen: change filter parameters courtesy of viking37.

--- drivers/input/touchscreen/lge_touch_core.c
+++ drivers/input/touchscreen/lge_touch_core.c
@@ -3766,10 +3766,10 @@ static int touch_probe(struct i2c_client *client, const struct i2c_device_id *id
 	if (ts->pdata->role->accuracy_filter_enable){
 		ts->accuracy_filter.ignore_pressure_gap = 5;
 		ts->accuracy_filter.delta_max = 30;
-		ts->accuracy_filter.max_pressure = 255;
-		ts->accuracy_filter.time_to_max_pressure = one_sec / 20;
-		ts->accuracy_filter.direction_count = one_sec / 6;
-		ts->accuracy_filter.touch_max_count = one_sec / 2;
+		ts->accuracy_filter.max_pressure = 55;
+		ts->accuracy_filter.time_to_max_pressure = 1;
+		ts->accuracy_filter.direction_count = 8;
+		ts->accuracy_filter.touch_max_count = 4;
 	}
 
 #if defined(CONFIG_HAS_EARLYSUSPEND)
